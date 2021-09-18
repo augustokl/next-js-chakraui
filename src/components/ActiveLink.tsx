9b5ca829663c369f3f2dd05922a6d1f92fa96df7
Augusto Kestering Longo <augusto.kestering@gmail.com>
2021-09-17 23:35:17 -0300

    Added react-hook-form and validation through yup

--- src/components/ActiveLink.tsx
+++ src/components/ActiveLink.tsx
@@ -1,30 +1,36 @@
-import { cloneElement, ReactElement } from 'react'
-import Link, { LinkProps } from 'next/link'
-import { useRouter } from 'next/router';
+import { cloneElement, ReactElement } from "react";
+import Link, { LinkProps } from "next/link";
+import { useRouter } from "next/router";
 
-interface ActiveLinkLinkProps extends LinkProps{
+interface ActiveLinkLinkProps extends LinkProps {
   children: ReactElement;
   shouldMatchExactHref: boolean;
 }
 
-export function ActiveLink({ children, shouldMatchExactHref = false, ...rest }: ActiveLinkLinkProps) {
-  const { asPath } = useRouter()
+export function ActiveLink({
+  children,
+  shouldMatchExactHref = false,
+  ...rest
+}: ActiveLinkLinkProps) {
+  const { asPath } = useRouter();
   let isActive = false;
 
-  if(shouldMatchExactHref && (asPath === rest.href || asPath === rest.as)) {
+  if (shouldMatchExactHref && (asPath === rest.href || asPath === rest.as)) {
     isActive = true;
   }
 
-  if(!shouldMatchExactHref && (asPath.startsWith(String(rest.href)) || asPath.startsWith(String(rest.as)))){
+  if (
+    !shouldMatchExactHref &&
+    (asPath.startsWith(String(rest.href)) || asPath.startsWith(String(rest.as)))
+  ) {
     isActive = true;
-
   }
 
-  return(
+  return (
     <Link {...rest}>
       {cloneElement(children, {
-        color: isActive ? 'pink.500' : 'gray.50'
+        color: isActive ? "pink.500" : "gray.50",
       })}
     </Link>
-  )
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,36 @@
-import { Flex, Button, Stack } from '@chakra-ui/react'
-import { Input } from '../components/Form/Input'
+import { Flex, Button, Stack } from "@chakra-ui/react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import * as Yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+import { Input } from "../components/Form/Input";
+
+type SignInFormData = {
+  email: string;
+  password: string;
+};
+
+const signInFormSchema = Yup.object().shape({
+  email: Yup.string().required().email(),
+  password: Yup.string().required(),
+});
 
 export default function SignIn() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm({
+    resolver: yupResolver(signInFormSchema),
+  });
+
+  const handleSignIn: SubmitHandler<SignInFormData> = async (values) => {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    console.log(values);
+  };
+
   return (
-    <Flex 
-      w="100vw"
-      h="100vh"
-      align="center"
-      justify="center"
-    >
+    <Flex w="100vw" h="100vh" align="center" justify="center">
       <Flex
         as="form"
         w="100%"
@@ -17,23 +39,34 @@ export default function SignIn() {
         p="8"
         borderRadius={8}
         flexDir="column"
+        onSubmit={handleSubmit(handleSignIn)}
       >
         <Stack spacing="4">
           <Input
             name="email"
             type="email"
             label="E-mail"
+            error={errors.email}
+            {...register("email")}
           />
           <Input
-            name="email"
-            type="email"
+            name="password"
+            type="password"
             label="Password"
+            error={errors.password}
+            {...register("password")}
           />
         </Stack>
-        <Button type="submit" mt="6" colorScheme="pink" size="lg">
+        <Button
+          type="submit"
+          mt="6"
+          colorScheme="pink"
+          size="lg"
+          isLoading={isSubmitting}
+        >
           Sign in
         </Button>
       </Flex>
     </Flex>
-  )
+  );
 }
